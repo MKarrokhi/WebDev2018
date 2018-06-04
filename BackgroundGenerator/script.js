6126d071cbedc24c2bde193d0abb6179ec7a6bff
Mustafa Karrokhi <mustafa.karrokhi@gmail.com>
2018-06-04 15:44:17 +0300

    Writing the generated css to the page

--- BackgroundGenerator/script.js
+++ BackgroundGenerator/script.js
@@ -12,6 +12,8 @@ var body = document.getElementById("grad");
 function setGradient(){
     body.style.background = "linear-gradient(to right, " + c1.value
     + ", " + c2.value + ")";
+
+    css.textContent = body.style.background + ";";
 }
 
 

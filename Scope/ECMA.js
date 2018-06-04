4418b1e21e3f4b3de3cb3313dd8f13b730aeebe8
Mustafa Karrokhi <mustafa.karrokhi@gmail.com>
2018-06-04 17:10:55 +0300

    New Excercises

---
+++ Scope/ECMA.js
@@ -0,0 +1,11 @@
+const player = 'bobby';
+let experience = 100;
+let wizardLevel = false;
+
+
+if (experience > 90){
+    let wizardLevel = true;
+    console.log('inside', wizardLevel);
+}
+
+console.log('outside', wizardLevel);
